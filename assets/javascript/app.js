769b0735b6f8e3d14b122382cd231bbdf5e5a9c3
Aizada Boposhova <aiszaman87@gmail.com>
2019-07-13 18:11:15 -0500

    clearn up code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,3 @@
-// pseudo code
-// shows only one question until the player answers it or their time runs out.
-// If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
-
-// The scenario is similar for wrong answers and time-outs.
-// If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
-// If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
-
-// On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
-
-//TODO fix the bug when correct/incorrect answer is displayed set to default
-
 $(document).ready(function() {
 
     // event listeners
